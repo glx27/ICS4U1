bb345dab76637540f3e7b3ae76ac161b32fb917c
glx27 <153835369+glx27@users.noreply.github.com>
2024-09-11 09:47:54 -0400

    colours

--- diceGame.java
+++ diceGame.java
@@ -1,63 +1,7 @@
-// import java.util.Random;
-// import java.util.Scanner;
-
-// public class Main {
-//     public static final String YELLOW = "\u001B[33m";
-//     public static final String RED = "\u001B[31m";
-//     public static final String RESET = "\u001B[0m";
-//     public static final String CYAN = "\u001B[36m";
-//     public static final String GREEN = "\u001B[32m";
-
-//     public static void main(String[] args) {
-//         Random random = new Random();
-//         int secretNumber = random.nextInt(100) + 1;
-//         Scanner scanner = new Scanner(System.in);
-//         int attempts = 0;
-//         int[] guesses = new int[5]; 
-
-//         while (attempts < 5) {
-//             int guess = guesser(scanner, secretNumber); 
-//             boolean alreadyGuessed = false;
-//             for (int i = 0; i < attempts; i++) {
-//                 if (guesses[i] == guess) {
-//                     alreadyGuessed = true;
-//                     break;
-//                 }
-//             }
-
-//             if (alreadyGuessed) {
-//                 System.out.println(RED + "You already guessed that number!" + RESET);
-//             } else {
-//                 guesses[attempts] = guess;
-//                 attempts++; 
-
-//                 if (guess == secretNumber) {
-//                     System.out.println(GREEN + "You guessed the number!" + RESET);
-//                     break;
-//                 } else if (guess < secretNumber) {
-//                     System.out.println(CYAN + "Too low! Try again." + RESET);
-//                 } else {
-//                     System.out.println(YELLOW + "Too high! Try again." + RESET);
-//                 }
-//             }
-//         }
-
-//         if (attempts == 5 && guesses[attempts - 1] != secretNumber) {
-//             System.out.println(RED + "You ran out of attempts. The secret number was " + secretNumber + RESET);
-//         }
-//     }
-
-//     static int guesser(Scanner scanner, int secretNumber) {
-//         System.out.print("Enter your guess: ");
-//         int guess = scanner.nextInt();
-//         return guess; 
-//     }
-// }*/
-
 import java.util.Scanner;
 import java.util.Random;
 
-public class Main {
+public class diceGame {
     public static final String PURPLE = "\u001B[35m";
     public static final String RED = "\u001B[31m";
     public static final String RESET = "\u001B[0m";
@@ -66,7 +10,7 @@ public class Main {
     public static void main(String[] args){
         Random random = new Random();
         Scanner scanner = new Scanner(System.in); 
-        int points = 500; 
+        int points = 500;       
         System.out.println(CYAN + "Welcome to the dice game! You have 500 points" + RESET);
         while (points > 0 && points <= 10000){
             int roll = random.nextInt(6) + 1;
